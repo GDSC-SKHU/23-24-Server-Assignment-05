bc8e0d43ae948159497c392e7220481bbe00ab7d
djdongjae <jk47wee@gmail.com>
2023-11-20 01:32:13 +0900

    Comment: 주석 추가

--- src/main/java/skhu/gdsc/securitypractice/config/JwtSecurityConfig.java
+++ src/main/java/skhu/gdsc/securitypractice/config/JwtSecurityConfig.java
@@ -9,7 +9,9 @@ import skhu.gdsc.securitypractice.jwt.JwtFilter;
 import skhu.gdsc.securitypractice.jwt.TokenProvider;
 
 @RequiredArgsConstructor
+// 보안 관련 설정을 커스터마이징 하기 위해 SecurityConfigurerAdapter을 상속 받음
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+  // JWT 토큰을 생성하고 검증하는 클래스의 인스턴스를 의존성 주입 받는다
   private final TokenProvider tokenProvider;
 
   @Override
